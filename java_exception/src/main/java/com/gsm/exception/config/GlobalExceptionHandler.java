968ab2aeb62f391e10f6ef6d7efd8a25aa7e3363
wjq <939949243@qq.com>
2020-09-14 13:11:02 +0800

    异常,处理数据校验

--- java_exception/src/main/java/com/gsm/exception/config/GlobalExceptionHandler.java
+++ java_exception/src/main/java/com/gsm/exception/config/GlobalExceptionHandler.java
@@ -3,12 +3,16 @@ package com.gsm.exception.config;
 import com.gsm.exception.entity.Code;
 import com.gsm.exception.entity.ExceptionResult;
 import com.gsm.exception.entity.R;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * 全局异常处理器
@@ -31,6 +35,20 @@ public class GlobalExceptionHandler {
                 .msg(Code.ERROR.msg)
                 .data(exceptionResult)
                 .build();
+
+        //数据校验 validated 异常
+        if (e instanceof MethodArgumentNotValidException) {
+            BindingResult br = ((MethodArgumentNotValidException) e).getBindingResult();
+            if (br.hasErrors()) {
+                List<ObjectError> errors = br.getAllErrors();
+                StringBuilder sb = new StringBuilder();
+                //多个数据校验不匹配
+                errors.forEach(i -> {
+                    sb.append(i.getDefaultMessage()).append(";");
+                });
+                exceptionResult.setMessage(sb.toString());
+            }
+        }
         return r;
     }
 }
