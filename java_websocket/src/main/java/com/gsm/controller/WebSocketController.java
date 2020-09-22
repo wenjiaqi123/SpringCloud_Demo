c2ea05650f494a2a6309413e1b8488bcafca8872
wjq <939949243@qq.com>
2020-09-22 21:16:27 +0800

    websocket

---
+++ java_websocket/src/main/java/com/gsm/controller/WebSocketController.java
@@ -0,0 +1,35 @@
+package com.gsm.controller;
+
+import com.gsm.server.WebSocketServer;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping("websocket")
+public class WebSocketController {
+
+    @GetMapping("/{cid}")
+    public String socket(@PathVariable("cid") String cid){
+        System.out.println("cid " + cid);
+        pushToWeb(cid,"我是客户端的信息");
+        return "success";
+    }
+
+    @ResponseBody
+    @RequestMapping("/socket/push/{cid}")
+    public Object pushToWeb(@PathVariable("cid") String cid,String msg){
+        System.out.println("--pushToWeb接口--");
+        //WebSocketServer.sendInfoAll(msg,cid);
+        for (int i = 0; i < 10; i++) {
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            WebSocketServer.sendInfoAll(new Date().toString(),cid);
+        }
+        return "aa";
+    }
+}
