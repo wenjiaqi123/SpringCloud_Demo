dfddba349f9375c6a2cda7d9b7bf08ab6b46bc65
wjq <939949243@qq.com>
2020-09-22 16:28:03 +0800

    job

---
+++ java_job/src/main/java/com/gsm/job/JobDemo.java
@@ -0,0 +1,22 @@
+package com.gsm.job;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
+
+@Configuration
+@EnableScheduling
+public class JobDemo {
+
+
+    // 网站地址 https://cron.qqe2.com/
+    @Scheduled(cron = "* * * * * ?")
+    //@Scheduled(cron = "${job.time}")
+    public void myJob() {
+        LocalDateTime now = LocalDateTime.now();
+        String time = now.getYear() + "年" + now.getMonthValue() + "月" + now.getDayOfMonth() + "日" + now.getHour() + "时" + now.getMinute() + "分" + now.getSecond() + "秒";
+        System.out.println(time);
+    }
+}
