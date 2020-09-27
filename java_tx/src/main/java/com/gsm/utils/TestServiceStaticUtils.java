c59f88055c442ccca22cc5e372a27a031c4bae9b
wjq <939949243@qq.com>
2020-09-27 17:01:02 +0800

    静态类中加载service

--- java_tx/src/main/java/com/gsm/utils/TestServiceStaticUtils.java
+++ java_tx/src/main/java/com/gsm/utils/TestServiceStaticUtils.java
@@ -15,18 +15,18 @@ public class TestServiceStaticUtils {
     private TxService txService;
 
     @PostConstruct
-    public void init(){
-        if(service==null){
+    public void init() {
+        if (service == null) {
             setService(txService);
         }
     }
 
-    private synchronized static void setService(TxService txService){
+    private synchronized static void setService(TxService txService) {
         service = txService;
     }
 
-    public static void test(){
+    public static void test() {
         List<Money> all = service.getAll();
-        System.out.println(all.size());
+        System.out.println("我被加载了" + all.size());
     }
 }
